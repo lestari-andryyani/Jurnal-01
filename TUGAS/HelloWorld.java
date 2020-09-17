0cfaf63c35568eee69c8397550fc6ccf84be99c0
lestari-andryyani <lestariandryyani2121@gmail.com>
2020-09-17 18:56:11 +0700

    HelloWorld.java

---
+++ TUGAS/HelloWorld.java
@@ -0,0 +1,9 @@
+    public class HelloWorld {
+	
+	    public static void main(String[] args) {
+		    System.out.println("Nama : Lestari Andryyani");
+			System.out.println("Nim : 6706202034");
+			System.out.println("Kelas : D3IF 44 04");
+		}
+	}
+	
